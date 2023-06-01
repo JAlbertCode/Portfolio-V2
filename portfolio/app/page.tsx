ecbf0e27a149691586c9c4b1b898ca606d3e7060
JAlbertCode <JonathanAlbert0115@gmail.com>
2023-06-01 16:11:01 -0400

    Added skills for mobile vendor, thought card, wix, blotto, livein, augmented piano lessons, and traffic jam vr. Updated the cards to have rounded borders.

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <div id='cards' className='flex flex-wrap gap-10 mt-10 place-content-center'>
 
           {/*This is the card for the invisible maze game*/}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/invisible-maze.png' alt='A view of an invisible maze game made in Decentraland'/>
             <CardContent>
               <Typography variant='h6'>
@@ -47,12 +47,13 @@ export default function Home() {
                 <Typography variant='body2' className='skills'>Decentraland</Typography>
                 <Typography variant='body2' className='skills'>MetaMask</Typography>
                 <Typography variant='body2' className='skills'>Notion</Typography>
+                <Typography variant='body2' className='skills'>Game Design</Typography>
               </div>
             </CardContent>
           </Card>
 
           {/* This is the card for Mix3d */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/mix3d.png' alt='A logo of circles in a spiral with varrying colors'/>
             <CardContent>
               <Typography variant='h6'>
@@ -71,12 +72,13 @@ export default function Home() {
                 <Typography variant='body2' className='skills'>Partybid</Typography>
                 <Typography variant='body2' className='skills'>Mirror</Typography>
                 <Typography variant='body2' className='skills'>MagicaVoxel</Typography>
+                <Typography variant='body2' className='skills'>Community Management</Typography>
               </div>
             </CardContent>
           </Card>
 
           {/* This is the card for Werewolf */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/werewolf.png' alt='A sign in screen with the word Werewolf at the top in a font that looks like bloody letters'/>
             <CardContent>
               <Typography variant='h6'>
@@ -109,7 +111,7 @@ export default function Home() {
           </Card>
 
           {/* This is the card for Grace Potter */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/grace-potter.png' alt='An icon of a pottery vase'/>
             <CardContent>
               <Typography variant='h6'>
@@ -143,7 +145,7 @@ export default function Home() {
           </Card>
 
           {/* This is the card for Mobile Vendor */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/mobile-vendor.png' alt='A smart phone overlayed on top of a map with icons for mobile vendors'/>
             <CardContent>
               <Typography variant='h6'>
@@ -155,11 +157,22 @@ export default function Home() {
               <Typography variant="body2" color="text.secondary">
                 August 2020
               </Typography>
+              <div className='gap-1 flex flex-wrap mt-1'>
+                <Typography variant='body2' className='skills'>Javascript</Typography>
+                <Typography variant='body2' className='skills'>React Native</Typography>
+                <Typography variant='body2' className='skills'>MapView</Typography>
+                <Typography variant='body2' className='skills'>Expo</Typography>
+                <Typography variant='body2' className='skills'>Redux</Typography>
+                <Typography variant='body2' className='skills'>HTML</Typography>
+                <Typography variant='body2' className='skills'>CSS</Typography>
+                <Typography variant='body2' className='skills'>Firebase</Typography>
+                <Typography variant='body2' className='skills'>Github</Typography>
+              </div>
             </CardContent>
           </Card>
 
           {/* This is the card for Thought Card */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/thought-card.png' alt="A blue business card with a quote that reads 'The convential mind is passive - it consumes information and regurgitates it in familiar forms. The dimensional mind is active. Transforming everything it digests into something new and original, creating instead of consuming' by Robert Greene from the book Mastery on page 177."/>
             <CardContent>
               <Typography variant='h6'>
@@ -171,11 +184,17 @@ export default function Home() {
               <Typography variant="body2" color="text.secondary">
                 April 2020
               </Typography>
+              <div className='gap-1 flex flex-wrap mt-1'>
+                <Typography variant='body2' className='skills'>Unity</Typography>
+                <Typography variant='body2' className='skills'>Vuforia</Typography>
+                <Typography variant='body2' className='skills'>C#</Typography>
+                <Typography variant='body2' className='skills'>Augmented Reality</Typography>
+              </div>
             </CardContent>
           </Card>
           
           {/* This is the card for Wix */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/wix.png' alt='The wix website logo'/>
             <CardContent>
               <Typography variant='h6'>
@@ -187,11 +206,18 @@ export default function Home() {
               <Typography variant="body2" color="text.secondary">
                 March 2020
               </Typography>
+              <div className='gap-1 flex flex-wrap mt-1'>
+                <Typography variant='body2' className='skills'>Wix</Typography>
+                <Typography variant='body2' className='skills'>Client Relations</Typography>
+                <Typography variant='body2' className='skills'>Business Analysis</Typography>
+                <Typography variant='body2' className='skills'>Project Management</Typography>
+                <Typography variant='body2' className='skills'>Budgeting</Typography>
+              </div>
             </CardContent>
           </Card>
 
           {/* This is the card for Blotto */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/blotto.png' alt="A flat image of a body with its head in a toilet and the words 'Blotto' written under the image."/>
             <CardContent>
               <Typography variant='h6'>
@@ -203,11 +229,20 @@ export default function Home() {
               <Typography variant="body2" color="text.secondary">
                 January 2020
               </Typography>
+              <div className='gap-1 flex flex-wrap mt-1'>
+                <Typography variant='body2' className='skills'>Unity</Typography>
+                <Typography variant='body2' className='skills'>C#</Typography>
+                <Typography variant='body2' className='skills'>UI</Typography>
+                <Typography variant='body2' className='skills'>Game Design</Typography>
+                <Typography variant='body2' className='skills'>Animation</Typography>
+                <Typography variant='body2' className='skills'>Sound</Typography>
+                <Typography variant='body2' className='skills'>Project Management</Typography>
+              </div>
             </CardContent>
           </Card>
 
           {/* This is the card for LiveIn */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/livein.png' alt='A logo of a leaf that has lines across it similar to that of a circuit board'/>
             <CardContent>
               <Typography variant='h6'>
@@ -219,11 +254,19 @@ export default function Home() {
               <Typography variant="body2" color="text.secondary">
                 October 2019
               </Typography>
+              <div className='gap-1 flex flex-wrap mt-1'>
+                <Typography variant='body2' className='skills'>Project Management</Typography>
+                <Typography variant='body2' className='skills'>Business Analysis</Typography>
+                <Typography variant='body2' className='skills'>UX</Typography>
+                <Typography variant='body2' className='skills'>Real Estate</Typography>
+                <Typography variant='body2' className='skills'>Documentation</Typography>
+                <Typography variant='body2' className='skills'>Augmented Reality</Typography>
+              </div>
             </CardContent>
           </Card>
 
           {/* This is the card for Augmented Piano Lessons */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/augmented-piano-lessons.png' alt='Piano keys on with a background of musical notes'/>
             <CardContent>
               <Typography variant='h6'>
@@ -235,11 +278,18 @@ export default function Home() {
               <Typography variant="body2" color="text.secondary">
                 December 2017
               </Typography>
+              <div className='gap-1 flex flex-wrap mt-1'>
+                <Typography variant='body2' className='skills'>Unity</Typography>
+                <Typography variant='body2' className='skills'>C#</Typography>
+                <Typography variant='body2' className='skills'>Augmented Reality</Typography>
+                <Typography variant='body2' className='skills'>Animation</Typography>
+                <Typography variant='body2' className='skills'>Sound</Typography>
+              </div>
             </CardContent>
           </Card>
 
           {/* This is the card for Traffic Jam VR */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/traffic-jam-vr.png' alt='A low poly image of a street corner'/>
             <CardContent>
               <Typography variant='h6'>
@@ -251,11 +301,18 @@ export default function Home() {
               <Typography variant="body2" color="text.secondary">
                 August 2017
               </Typography>
+              <div className='gap-1 flex flex-wrap mt-1'>
+                <Typography variant='body2' className='skills'>Unity</Typography>
+                <Typography variant='body2' className='skills'>C#</Typography>
+                <Typography variant='body2' className='skills'>Virtual Reality</Typography>
+                <Typography variant='body2' className='skills'>Game Design</Typography>
+                <Typography variant='body2' className='skills'>Scene Design</Typography>
+              </div>
             </CardContent>
           </Card>
 
           {/* This is the card for The Fourth Branch */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/the-fourth-branch.png' alt='A logo with two parallel blue lines at the forefront and a red line in the background in the shape of a staple'/>
             <CardContent>
               <Typography variant='h6'>
@@ -271,7 +328,7 @@ export default function Home() {
           </Card>
 
           {/* This is the card for Memorial Website */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/memorial-website.jpg' alt="A painted mural of a person with a hat, and the word 'Ferle' written under it"/>
             <CardContent>
               <Typography variant='h6'>
@@ -287,7 +344,7 @@ export default function Home() {
           </Card>
 
           {/* This is the card for Customizable Phone Case */}
-          <Card sx={{width: 357}}>
+          <Card sx={{width: 357, borderRadius: 3}}>
             <CardMedia component="img" image='/images/custom-phone-case.png' alt="A mechanical engineering mockup of an old iphone with a case"/>
             <CardContent>
               <Typography variant='h6'>
