221c0b29514b7ec82b1b4dda22e7cbbc6eb92725
JAlbertCode <JonathanAlbert0115@gmail.com>
2023-06-05 11:16:07 -0400

    Updated the home page with the personality profile and logical ability test results.

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -11,6 +11,16 @@ export default function Home() {
         {/*This is the introduction to the landing page*/}
         <h1>Jonathan Albert's Portfolio</h1>
         <p>I am a futurist, reverse engineering the products and services of tomorrow today. Turning visions into projects, projects into milestones, and milestones into tasks providing software development teams with roadmaps to the future. Nine years of experience gathering requirements, rapid prototyping, managing, and coordinating cross-functional teams in both start-up and global operations. Boundless curiosity has led to work with organizations focused on blockchains, gaming, consulting, augmented reality, finance, politics, and language learning. Intent on bridging the gap between the future and today, confronting any challenge that gets in the way. </p>
+        <br/>
+        <p>Want to know me on a more personal level? View my Alva Labs test results</p>
+        <div className="flex gap-2">
+          <div className='links'>
+          <Link target='_blank' href="/documents/Jonathan_ALbert_Personality_Test_Five_Factor_Personality_Theory.pdf">Personality Profile</Link>
+          </div>
+          <div className='links'>
+          <Link target='_blank' href="/documents/Jonathan_Albert_Logical_Ability.pdf">Logical Ability</Link>
+          </div>
+        </div>
         {/* <h2>Get in Contact</h2> */}
         {/*This div conatains the row of icons for professional and social media links*/}
         <div className='flex flex-wrap gap-4 mt-10'>
